28acd39b5581b76fa71bc22ca7ceafde78510d38
Caleb <calebfunderburk@icloud.com>
2022-10-09 13:04:11 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// Dependencies
+const express = require('express')
+
+// Modular code
+const sequelize = require('./config/connection')
+
+// Format express
+const app = express()
+const PORT = process.env.PORT || 3001
+
+// Middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'))
+  })
