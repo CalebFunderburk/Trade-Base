69c12b1c3ab7306d24fcf962b6dff949428518f8
Caleb <calebfunderburk@icloud.com>
2022-10-15 14:13:10 -0400

    updated models

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 // Modular code
 const sequelize = require('./config/connection')
+const routes = require('./routes')
 
 // Format express
 const app = express()
@@ -12,7 +13,10 @@ const PORT = process.env.PORT || 3001
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+// Turn on routes
+app.use(routes)
+
 // Turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening'))
-  })
+})
