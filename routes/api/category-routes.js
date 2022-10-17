bb2ea68804eaf01c3afb2c8a4a8669f6be70d53d
Caleb <calebfunderburk@icloud.com>
2022-10-17 16:17:24 -0400

    added tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,6 +3,7 @@ const { Category, Product } = require('../../models');
 
 // Get all categories
 router.get('/', (req, res) => {
+  // Find all categories and include the following properties
   Category.findAll({
     include: [
       {
@@ -11,6 +12,7 @@ router.get('/', (req, res) => {
       }
     ]
   })
+  // Return the promise as json
   .then(categoryData => res.json(categoryData))
   .catch(err => {
     console.log(err)
@@ -20,6 +22,7 @@ router.get('/', (req, res) => {
 
 // Get one category
 router.get('/:id', (req, res) => {
+  // Find one category that meets the following criteria
   Category.findOne({
     where: {
       id: req.params.id
@@ -32,10 +35,12 @@ router.get('/:id', (req, res) => {
     ]
   })
   .then(categoryData => {
+    // Display an error if there is no such category with the specified id
     if (!categoryData) {
       res.status(404).json({ message: 'No category found with this id' })
       return
     }
+    // Return the promise as json
     res.json(categoryData)
   })
   .catch(err => {
@@ -46,9 +51,11 @@ router.get('/:id', (req, res) => {
 
 // Create a category
 router.post('/', (req, res) => {
+  // Create a category with the defined data
   Category.create({
     category_name: req.body.category_name
   })
+  // Return the promise as json
   .then(categoryData => res.json(categoryData))
   .catch(err => {
     console.log(err)
@@ -56,17 +63,21 @@ router.post('/', (req, res) => {
   })
 });
 
+// Update a category
 router.put('/:id', (req, res) => {
+  // Update a category that meets the following cirteria
   Category.update(req.body, {
     where: {
       id: req.params.id
     }
   })
   .then(categoryData => {
+    // Display an error if there is no such category with the specified id
     if(!categoryData[0]) {
       res.status(404).json({ message: 'No category found with this id' })
       return
     }
+    // Return the promise as json
     res.json(categoryData)
   })
   .catch(err => {
@@ -75,17 +86,21 @@ router.put('/:id', (req, res) => {
   })
 });
 
+// Delete a category
 router.delete('/:id', (req, res) => {
+  // Delete a category that meets the following criteria
   Category.destroy({
     where: {
       id:req.params.id
     }
   })
   .then(categoryData => {
+    // Display an error if there is no such category with the specified id
     if (!categoryData) {
       res.status(404).json({ message: 'No category found with this id' })
       return
     }
+    // Return the promise as json
     res.json(categoryData)
   })
   .catch(err => {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,112 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
-
+// Get all tags
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  // Find all tags and include the following properties
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  // Return the promise as json
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
+// Get one tag
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  // Find one tag that meets the following criteria
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(tagData => {
+    // Display an error if there is no such tag with the specified id
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id' })
+      return
+    }
+    // Return the promise as json
+    res.json(tagData)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
+// Create a tag
 router.post('/', (req, res) => {
-  // create a new tag
+  // Create a tag with the defined data
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  // Return the promise as json
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
+// Update a tag
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  // Update a tag that meets the following criteria
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(tagData => {
+    // Display an error if there is no such category with the specified id
+    if (!tagData[0]) {
+      res.status(404).json({ message: 'No category found with this id' })
+      return
+    }
+    // Return the promise as json
+    res.json(tagData)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
+// Delete a tag
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  // Delete a tag that meets the following criteria
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(tagData => {
+    // Display an error if there is no such category with the specified id
+    if (!tagData) {
+      res.status(404).json({ message: 'No category found with this id' })
+      return
+    }
+    // Return the promise as json
+    res.json(tagData)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 module.exports = router;
